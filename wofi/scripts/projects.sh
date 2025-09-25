04eb527f231aa50efb5f74e8ac0a662a611e03b9
Widua <mikolaj.widla@gmail.com>
2025-09-25 21:03:16 +0200

    Added custom rules, and improved script

--- wofi/scripts/projects.sh
+++ wofi/scripts/projects.sh
@@ -4,5 +4,6 @@ folder=$(find ~/Projects -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | sort |
 
 [ -n "$folder" ] || exit 0
 
-kitty nvim ~/Projects/$folder & disown 
-
+kitty ~/Projects/$folder &disown
+wl-copy ~/Projects/$folder & disown
+kitty --class nvim -d ~/Projects/$folder nvim 
