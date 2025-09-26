fc071949eb51ace336ebec2ec8e4c74cfeba6cc5
Widua <mikolaj.widla@gmail.com>
2025-09-26 18:08:08 +0200

    Updated projects script, and added template for desktop

--- wofi/scripts/projects.sh
+++ wofi/scripts/projects.sh
@@ -1,9 +1,12 @@
 #!/bin/zsh
 
-folder=$(find ~/Projects -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | sort | wofi --show dmenu --prompt "Select project:")
+folder=$(find ~/ -mindepth 2 -maxdepth 2 -type d -regex '.*/[^/]*Projects/[^/]*' \
+     -printf "%P\n" \
+    | sort \
+    | wofi --show dmenu --prompt "Select project:")
 
 [ -n "$folder" ] || exit 0
 
-kitty ~/Projects/$folder &disown
-wl-copy ~/Projects/$folder & disown
-kitty --class nvim -d ~/Projects/$folder nvim 
+kitty "$folder" & disown
+wl-copy "$folder" & disown
+kitty --class nvim -d "$folder" nvim
